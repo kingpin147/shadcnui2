48e130f01b9b24c8dcb153ded5748a8fca599b9b
Muhammad Nouman Attique <108257688+kingpin147@users.noreply.github.com>
2024-01-10 16:06:04 +0500

    update

---
+++ src/components/card.tsx
@@ -0,0 +1,17 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+  } from "@/components/ui/card"
+  
+
+const CardDemo = () => {
+  return (
+    <div>CardDemo</div>
+  )
+}
+
+export default CardDemo
