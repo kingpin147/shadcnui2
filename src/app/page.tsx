b3d446b06e6f5dae9af65363603004a77b61a886
Muhammad Nouman Attique <108257688+kingpin147@users.noreply.github.com>
2024-01-12 12:26:48 +0500

    calendar component update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,26 +2,53 @@ import { AccordionDemo } from "@/components/accordian";
 import { AlertDialogDemo } from "@/components/alert-dialogue";
 import { CarouselDemo } from "@/components/caraosal";
 import CardDemo from "@/components/card";
+import { TypographyH1 } from "@/components/heading";
+
+import { Calendar } from "@/components/ui/calendar";
+
+
 import Image from "next/image";
+import { ButtonSecondary } from './../components/button';
 
 export default function Home() {
   return (
     <>
       <main className="flex  flex-col items-center justify-between p-8 bg-slate-500">
+        {/* caraousel */}
         <CarouselDemo />
         <div className="p-8 bg-slate-500">
           {" "}
+          {/* alert dialog */}
           <AlertDialogDemo />
         </div>
       </main>
       <div className="p-8 bg-slate-500">
         {" "}
+        {/* accordion */}
         <AccordionDemo  />
       </div>
       <div className=" flex justify-center item-center p-8 bg-slate-500">
         {" "}
+        {/* profile Card */}
         <CardDemo/>
       </div>
+      {/* calerdar ui */}
+      {/* main div */}
+      <div className="grid  grid-cols-2 gap-4 p-5  m-7">
+        {/* heading div */}
+        <div className=" flex flex-col gap-8 justify-center items-center p-5">
+          <TypographyH1/>
+          <div className="flex justify-center items-center gap-8">
+            <ButtonSecondary text={"Search"} />
+            <ButtonSecondary text={"Profile"} />
+          </div>
+        </div>
+        {/* calender div */}
+        <div className="flex justify-center items-center p-5 ">
+          <Calendar/>
+         
+          </div>
+        </div>
     </>
   );
 }
---
+++ src/components/button.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@/components/ui/button"
+
+export function ButtonSecondary({text}:{text:string}) {
+  return <Button variant="secondary">{text}</Button>
+}
